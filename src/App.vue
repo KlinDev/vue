c34ef5c77a5dc05e12557bf6a90399f0796b5ee2
Klindev <pashaseo717@gmail.com>
2021-09-27 00:18:47 +0300

    call the templated table component 2 times in the app

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,109 @@
 <template>
   <div id="app">
+
     <h1>Click by table row</h1>
-<!--    <hr />-->
-    <Tables
-      v-bind:secondTableData="secondTableData"
-      v-bind:firstTableData="firstTableData"
-      v-on:moved-second-table-row="movedSecondTableRow"
-      v-on:moved-first-table-row="movedFirstTableRow"
-    />
+
+    <div class="tables">
+      <Table
+              v-bind:TableData="firstTableData"
+              v-on:row-table-move="movedFirstTableRow"
+      />
+      <Table
+              v-bind:TableData="secondTableData"
+              v-on:row-table-move="movedSecondTableRow"
+      />
+    </div>
+    <!-- /.tables -->
+
   </div>
 </template>
 
 <script>
-import firstTableUsers from "./firstTableData.json";
-import Tables from "@/components/Tables";
+  import firstTableUsers from "./firstTableData.json";
+  import Table from "@/components/Table";
 
-export default {
-  name: "App",
-  data() {
-    return {
-      secondTableData: [
-        { name: "Paul", id: 6 },
-        { name: "Mike", id: 7 },
-        { name: "Alex", id: 8 },
-        { name: "Natasha", id: 9 },
-        { name: "Katya", id: 10 },
-      ],
-      firstTableData: firstTableUsers,
-    };
-  },
-  mounted() {
-    // this.firstUsers = JSON.parse(firstTableUsers);
-    // // this.movedFirstTableRow();
-    // this.movedSecondTableRow();
-  },
-  methods: {
+  export default {
 
-    movedFirstTableRow(name, index2) {
-      let firstArrayItem = this.firstTableData[index2];
-      this.firstTableData = this.firstTableData.filter(t => t.name !== name);
-      this.secondTableData.push(firstArrayItem);
+    name: "App",
+
+    data() {
+      return {
+        secondTableData: [
+          { name: "Paul", id: 6 },
+          { name: "Mike", id: 7 },
+          { name: "Alex", id: 8 },
+          { name: "Natasha", id: 9 },
+          { name: "Katya", id: 10 },
+        ],
+        firstTableData: firstTableUsers,
+      };
     },
-    movedSecondTableRow(name, index) {
-      let secondArrayItem = this.secondTableData[index];
-      this.secondTableData = this.secondTableData.filter(t => t.name !== name);
-      this.firstTableData.push(secondArrayItem);
+
+    methods: {
+      movedFirstTableRow(name, index) {
+        const firstArrayItem = {...this.firstTableData[index]};
+        this.firstTableData = this.firstTableData.filter(i => i.name !== name);
+        this.secondTableData.push(firstArrayItem);
+      },
+      movedSecondTableRow(name, index) {
+        const secondArrayItem = {...this.secondTableData[index]};
+        this.secondTableData = this.secondTableData.filter(i => i.name !== name);
+        this.firstTableData.push(secondArrayItem);
+      }
     },
-  },
-  components: {
-    Tables,
-  },
-};
+
+    components: {
+      Table,
+    },
+
+  };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap');
-
-
-#app {
-  font-family: 'Red Hat Display', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1 {
-  color: #f3f3f3;
-}
-.tables {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-.table {
-  margin: 10px;
-  color: #000;
-}
-.table th,
-.table td {
-  padding: 10px 20px;
-  border: 2px solid #fff;
-}
-table.table tr{
-  transition: .4s cubic-bezier(0.55, 0.43, 0.18, 1.54);
-}
-table.table tr:not(:first-child):hover {
-  background: #e3e3e3;
-  transform: scale(.9);
-  border: none;
-}
-.table td {
-  cursor: pointer;
-}
-.table .cell-name {
-  width: 150px;
-}
-table.table tr:not(:first-child) {
-  background: #fbfbfb;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap');
+  #app {
+    font-family: 'Red Hat Display', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1 {
+    color: #f3f3f3;
+  }
+  .tables {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  .table {
+    margin: 10px;
+    color: #000;
+  }
+  .table th,
+  .table td {
+    padding: 10px 20px;
+    border: 2px solid #fff;
+  }
+  table.table tr{
+    display: table;
+    width: 100%;
+    text-align: center;
+    transition: .4s cubic-bezier(0.55, 0.43, 0.18, 1.54);
+  }
+  table.table tr:not(:first-child):hover {
+    background: #e3e3e3;
+    transform: scale(.9);
+    border: none;
+  }
+  .table td {
+    cursor: pointer;
+  }
+  .table .cell-name {
+    width: 150px;
+  }
+  table.table tr:not(:first-child) {
+    background: #fbfbfb;
+  }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tables">
+
+    <!-- таблица  -->
+    <table cellpadding="0" cellspacing="0" class="table">
+      <tbody>
+      <tr>
+        <th class="cell-name"> User name</th>
+        <th>ID</th>
+      </tr>
+      <tr
+              v-for="(tableRow, index) in TableData"
+              :key="tableRow.id"
+              v-on:click="$emit('row-table-move', tableRow.name, index)"
+      >
+        <td class="cell-name">
+          {{tableRow.name}}</td>
+        <td class="cell-id">{{tableRow.id}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Tables',
+
+    props: {
+      TableData: {
+        type: Object,
+        required: true,
+      },
+      index: Number,
+    },
+
+    methods: {
+      movedTableRow(name, index) {
+        this.$emit('row-table-move', name, index)
+      },
+    }
+  }
+</script>
